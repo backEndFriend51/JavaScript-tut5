db46a40957d153426bb3bae4d55700a3cc33e049
backEndFriend51 <59559655+backEndFriend51@users.noreply.github.com>
2020-03-27 11:36:45 +0100

    Add js- file via uplaod
    arrayUm.js contains the array- meinArray one for- loop with alert() and one for- loop with alert(meinArray.reverse()); also alert(meinArray.sort());

---
+++ arrayUm.js
@@ -0,0 +1,11 @@
+var meinArray = ["a", "c", "b", "d", "f", "g", false];
+
+for(var i = 0; i < meinArray.length; i++) {
+			alert("Der " + (i+1) + ". Wert (des Arrays) beim Index " + i + " = " + meinArray[i]);
+		} 
+		
+for(var i = 0; i < meinArray.length; i++) {
+			alert(meinArray.reverse());
+		} 
+		
+			alert(meinArray.sort());
